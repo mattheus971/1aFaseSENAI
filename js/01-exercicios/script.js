15c460e3c5c09375ade907e630877a0bf00652bd
mattheus971 <mathvelozo2000@gmail.com>
2025-04-17 21:02:02 -0300

    Adicionei botões que fazem contas

---
+++ js/01-exercicios/script.js
@@ -0,0 +1,40 @@
+
+function somar() {
+
+    let numeroUm = Number(prompt("Digite um número: "))
+    let numeroDois = Number(prompt("Digite outro número: "))
+
+    let soma = numeroUm + numeroDois
+
+    alert("A soma é: " + soma)
+}
+
+function subtrair() {
+
+    let numeroUm = Number(prompt("Digite um número: "))
+    let numeroDois = Number(prompt("Digite outro número: "))
+
+    let subtracao = numeroUm - numeroDois
+
+    alert("A soma é: " + subtracao)
+}
+
+function multiplicar() {
+
+    let numeroUm = Number(prompt("Digite um número: "))
+    let numeroDois = Number(prompt("Digite outro número: "))
+
+    let multiplicacao = numeroUm * numeroDois
+
+    alert("A soma é: " + multiplicacao)
+}
+
+function dividir() {
+
+    let numeroUm = Number(prompt("Digite um número: "))
+    let numeroDois = Number(prompt("Digite outro número: "))
+
+    let divisao = numeroUm / numeroDois
+
+    alert("A soma é: " + divisao)
+}
