788f81fad20c92515843a8cd7b8963a1df90934e
mattheus971 <mathvelozo2000@gmail.com>
2025-05-22 22:03:24 -0300

    Adicionei novos exercicios

---
+++ js/exercicio-profRafael/telas/script.js
@@ -0,0 +1,10 @@
+function mostrarLogin(){
+    document.getElementById('container-login').style.display = "flex"
+    document.getElementById('container-cadastro').style.display = "none"
+
+}
+
+function mostrarCadastro(){
+    document.getElementById('container-cadastro').style.display = "flex"
+    document.getElementById('container-login').style.display = "none"
+}
