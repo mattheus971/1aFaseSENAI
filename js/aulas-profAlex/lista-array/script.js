979ef1d506af2e95b30d7dcee588a8592a5cea79
mattheus971 <mathvelozo2000@gmail.com>
2025-06-10 21:47:18 -0300

    Adicionei nova lista de exercicios

---
+++ js/aulas-profAlex/lista-array/script.js
@@ -0,0 +1,23 @@
+let nomes = ['Matheus', 'Lucas', 'Macal']
+
+// percorre o array inteiro como for
+// nomes.forEach((element, index) => {
+//     console.log("Olá" + element + index)
+// })
+
+
+// criar um copia com modificações do array
+// nomesComSobrenome = nomes.map((nome) => {
+//     return  nome + ' da Silva'
+// })
+// console.log(nomesComSobrenome)
+
+
+// busca pela informação que 
+// let nomesFiltrados = nomes.filter((nome) => nome.length > 5)
+// let findNome = nomes.filter((nomes) => nome.length > 5)
+// console.log(findNome);
+
+let numero = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+let numerosInverso = numeros.map()
